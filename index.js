f4b45dc075810b7d32b0605b1843363e456330a2
skalum <stkalum@gmail.com>
2017-09-27 21:32:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ function logShout (string) {
 function logWhisper (string) {
   console.log(whisper(string))
 }
+
+function sayHiToGrandma (string) {
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  } else if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  } else if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+}
