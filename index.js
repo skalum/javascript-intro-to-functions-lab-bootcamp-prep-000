eb0f6b4b880813a56588a01a5e09087b2a1cfb4c
skalum <stkalum@gmail.com>
2017-09-27 21:33:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function logWhisper (string) {
 function sayHiToGrandma (string) {
   if (string === "I love you, Grandma.") {
     return "I love you, too."
-  } else if (string.toUpperCase() === string) {
+  else if (string.toUpperCase() === string)
     return "YES INDEED!"
-  } else if (string.toLowerCase() === string) {
+  else if (string.toLowerCase() === string) {
     return "I can't hear you!"
   }
 }
