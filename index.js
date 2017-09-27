cf526667b8af5cf0298d3ea3a5fb4f123188d438
skalum <stkalum@gmail.com>
2017-09-27 21:33:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma (string) {
     return "I love you, too."
   else if (string.toUpperCase() === string)
     return "YES INDEED!"
-  else if (string.toLowerCase() === string) {
+  else if (string.toLowerCase() === string)
     return "I can't hear you!"
   }
 }
